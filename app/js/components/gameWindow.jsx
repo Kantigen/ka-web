c17be86b53b6ed37ee8eca7b9256daa29b4a469e
Nathan McCallum <nathan@billowsoftware.com.au>
2019-10-17 14:06:26 +1000

    Convert <GameWindow> to a functional component

--- app/js/components/gameWindow.jsx
+++ app/js/components/gameWindow.jsx
@@ -1,54 +1,50 @@
-'use strict';
+const React = require('react');
+const ReactTooltip = require('react-tooltip');
 
-var React = require('react');
-var ReactTooltip = require('react-tooltip');
+const LeftSidebar = require('js/components/menu/leftSidebar');
+const RightSidebar = require('js/components/menu/rightSidebar');
+const Map = require('js/components/menu/map');
+const Menu = require('js/components/menu');
+const WindowManager = require('js/components/windowManager');
 
-var LeftSidebar = require('js/components/menu/leftSidebar');
-var RightSidebar = require('js/components/menu/rightSidebar');
-var Map = require('js/components/menu/map');
-var Menu = require('js/components/menu');
-var WindowManager = require('js/components/windowManager');
+const MailWindow = require('js/components/window/mail');
+const OptionsWindow = require('js/components/window/options');
+const StatsWindow = require('js/components/window/stats');
 
-var MailWindow = require('js/components/window/mail');
-var OptionsWindow = require('js/components/window/options');
-var StatsWindow = require('js/components/window/stats');
-
+//
 // This React component will be the main container of everything that appears on the screen.
+//
 
-var GameWindow = React.createClass({
-    render: function() {
-        return (
-            <div
-                id='sidebarContainer'
-                style={{
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    width: '100%',
-                    height: '100%',
-                }}
-            >
-                <LeftSidebar />
-                <RightSidebar />
+const GameWindow = () => (
+    <div
+        id='sidebarContainer'
+        style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+        }}
+    >
+        <LeftSidebar />
+        <RightSidebar />
 
-                {/* One container to rule them all... */}
-                <div className='pusher'>
-                    {/*
+        {/* One container to rule them all... */}
+        <div className='pusher'>
+            {/*
                         This sets all the tooltips in the entire client.
                         See http://npmjs.org/package/react-tooltip for usage.
                     */}
-                    <ReactTooltip effect='solid' place='bottom' type='dark' />
-                    <Menu />
-                    <Map />
-                    <div id='content'></div> {/* This div is used by map. */}
-                    <WindowManager />
-                    <MailWindow />
-                    <OptionsWindow />
-                    <StatsWindow />
-                </div>
-            </div>
-        );
-    },
-});
+            <ReactTooltip effect='solid' place='bottom' type='dark' />
+            <Menu />
+            <Map />
+            <div id='content'></div> {/* This div is used by map. */}
+            <WindowManager />
+            <MailWindow />
+            <OptionsWindow />
+            <StatsWindow />
+        </div>
+    </div>
+);
 
 module.exports = GameWindow;
