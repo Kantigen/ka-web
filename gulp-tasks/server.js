c853fdaa56d97e9bbe6f0d99e0382324baaf684d
Nathan McCallum <nathan@1vas.me>
2020-03-18 07:26:37 +1000

    Add Lacuna-Assets as a submodule. Update references to assets.
    My editor decided to reformat the entire legacy-styles.css file - I feel like a noob. Sorry!

--- gulp-tasks/server.js
+++ gulp-tasks/server.js
@@ -6,10 +6,12 @@ var path = require('path');
 module.exports = function(done) {
     var app = express();
     var port = process.env.PORT || 80;
-    app.use(express.static(path.join(__dirname, '..')));
+    var dir = path.join(__dirname, '..');
+    console.log('Servering static files from', dir);
+    app.use(express.static(dir));
 
     app.listen(port, function() {
-        console.log('Listening on http://localhost:' + port + ' for requests.');
+        console.log(`Listening on http://localhost:${port}/ for requests.`);
         done();
     });
 };
