18143a1d2b137c2448ff84f4daf6c16e12bc1ffb
Nathan McCallum <hey@nathan-mccallum.com>
2022-04-19 13:58:55 +1000

    Rogue console call

--- app/js/components/window/stats.jsx
+++ app/js/components/window/stats.jsx
@@ -4,7 +4,6 @@ import StatsWindowStore from 'app/js/stores/window/stats';
 
 class StatsWindow extends React.Component {
     render() {
-        console.log('statswindow', StatsWindowStore.shown);
         if (StatsWindowStore.shown) {
             YAHOO.lacuna.Stats.show();
         }
