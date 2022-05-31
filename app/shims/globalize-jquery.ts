bbc1e48f8ae3f31226fd5da9ba13c8f181e121c8
Nathan McCallum <hey@nathan-mccallum.com>
2022-05-31 21:54:14 +1000

    Ignore global $ variable issues, fixes TS compilation error

--- app/shims/globalize-jquery.ts
+++ app/shims/globalize-jquery.ts
@@ -1,3 +1,4 @@
 import $ from 'jquery';
+// @ts-expect-error
 window.$ = window.jQuery = $;
 export default $;
--- app/shims/jquery.ts
+++ app/shims/jquery.ts
@@ -1,4 +1,5 @@
 import 'app/shims/globalize-jquery';
 import 'app/shims/fomantic';
+// @ts-expect-error
 const $ = window.$;
 export default $;
