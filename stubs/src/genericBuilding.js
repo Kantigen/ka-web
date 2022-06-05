c99f8ffce7837981ccdf4c626ea753ffd98c4688
Nathan McCallum <hey@nathan-mccallum.com>
2022-06-05 13:11:00 +1000

    Stubbed building view method now tries to return relevant building data

--- stubs/src/genericBuilding.js
+++ stubs/src/genericBuilding.js
@@ -1,8 +1,26 @@
 import Buildings from './buildings.js';
+import Body from './body.js';
+
+const { buildings } = Body.get_buildings();
 
 const GenericBuilding = {
-  view() {
-    return Buildings.view();
+  view(req, res) {
+    const response = Buildings.view();
+    const id = req.body.params[1];
+    const b = buildings[id];
+
+    if (b) {
+      response.building.id = id;
+      response.building.x = b.x;
+      response.building.y = b.y;
+      response.building.efficiency = b.efficiency;
+      response.building.name = b.name;
+      response.building.image = b.image;
+      response.building.level = b.level;
+      response.building.url = b.url;
+    }
+
+    return response;
   },
 };
 
